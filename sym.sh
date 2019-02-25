29d6c54bafbd72e0e6d082b0cc50a56c1c77b3c4
Thomas E <slashdevsda@gmail.com>
2019-02-25 23:46:47 +0100

    add utility script (symlinks)

---
+++ sym.sh
@@ -0,0 +1,21 @@
+# creates symlink to this repository, backup previous config.
+
+mkdir -p ./backup
+cp ~/.emacs ./backup/emacs
+cp ~/.xmobarrc ./backup/xmobarrc
+cp ~/.xmonad/xmonad.hs ./backup/xmonad.hs
+cp ~/.tmux.conf ./backup/xmonad.hs
+
+
+rm ~/.xmobarrc 
+rm ~/.emacs 
+rm ~/.xmonad/xmonad.hs
+rm ~/.tmux.conf
+
+
+ln -s $(pwd)/emacs_config.el ~/.emacs 
+ln -s $(pwd)./xmobarrc ~/.xmobarrc 
+ln -s $(pwd)./xmonad.hs ~/.xmonad/xmonad.hs
+ln -s $(pwd)./tmux.conf ~/.tmux.conf
+
+
